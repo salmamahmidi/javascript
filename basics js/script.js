9778c737027e37432daf6f0136c6ace41a2fb841
ghareb <salmamahmidi5@gmail.com>
2024-07-18 12:17:20 +0100

    user

---
+++ basics js/script.js
@@ -0,0 +1,8 @@
+var userName=prompt('entrer your name');
+var age=prompt('entrer your age');
+var x=prompt('entrer x');
+var y=prompt('entrer y');
+console.log('x=',x);
+console.log('y=',y);
+console.log('x>y',x>y);
+console.log('hello'+userName+' you are '+age);
